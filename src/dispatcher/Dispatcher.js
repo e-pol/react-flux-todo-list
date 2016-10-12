5f2137585e433b74f3bb9c1b60013d050deb6489
e.pol <eugene.poletski@gmail.com>
2016-10-12 10:57:33 +0300

    Add TodoStore

--- src/dispatcher/Dispatcher.js
+++ src/dispatcher/Dispatcher.js
@@ -1,7 +1,7 @@
 import ES6Promise from 'es6-promise';
 import assign from 'object-assign';
 
-var Promise = ES6Promise.Promise;
+const Promise = ES6Promise.Promise;
 var _callbacks = [];
 var _promises = [];
 
